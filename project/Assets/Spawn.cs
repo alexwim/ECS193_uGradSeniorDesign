ed83bcf7825428da35d9376bd2dd3fbb408bc850
Mario Yepez <myepez@ucdavis.edu>
2015-02-26 22:26:53 -0800

    Added A* Pathfinding Package Added Basic Spawner to main scene Added Basic Enemy  + assorted scripts to main scene
    Former-commit-id: a8ef1f88f595b5778ca33d59f9015139063b8395

---
+++ project/Assets/Spawn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	public Transform target;
+	public GameObject unit;
+	
+	public float spawnTime = .5f;
+	float spawnTimeLeft = .5f;
+
+
+	// Update is called once per frame
+	void Update () {
+		if(spawnTimeLeft <= 0) {
+			GameObject go = (GameObject)Instantiate(unit, transform.position, transform.rotation);
+			go.GetComponent<AstarAI>().target = target;
+			spawnTimeLeft = spawnTime;
+		}
+		else {
+			spawnTimeLeft -= Time.deltaTime;
+		}
+	}
+}
