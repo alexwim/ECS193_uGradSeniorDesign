8565e5d46db973c207fbc1adecf541bcd569e69d
Timothy Tong <tktong418@gmail.com>
2015-02-28 17:22:40 -0800

    Added AI attack.
    Former-commit-id: 0c5ecf064f68215598d0c859aecf05fcd318a2a6

---
+++ project/Assets/Custom/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+	public int health;
+	public int maxHealth = 100;
+
+	private bool isDead;
+
+	private void Awake() {
+		health = maxHealth;
+		isDead = false;
+	}
+
+	public void TakeDamage(int amount) {
+		health -= amount;
+	}
+
+	private void Death() {
+		isDead = true;
+	}
+}
