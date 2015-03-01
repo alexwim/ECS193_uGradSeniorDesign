c97b854801cc06d78cd9e1bb377e47d60dd55ee5
Timothy Tong <tktong418@gmail.com>
2015-02-28 16:57:00 -0800

    Updated directory to work with navmesh and prototype.unity.
    Former-commit-id: fb2e10dd659b54fd70c7937a89ce0f6b1c073108

---
+++ project/Assets/Custom/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	private Transform player;
+	private NavMeshAgent navMeshAgent;
+
+	private void Awake() {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+	}
+
+	private void Update() {
+		navMeshAgent.SetDestination (player.position);
+	}
+}
