2f7ff0c7cac9fb023a77117f59a6f956ff22119c
Alexandre Wimmers <adwimmers@ucdavis.edu>
2015-04-28 09:28:40 -0700

    Added working start button.
    Former-commit-id: db841178c75947cf2a3fd3ea1beaec05925dcd6f

--- project-unity5/Assets/Custom/Scripts/HUD/StartButton.cs
+++ project-unity5/Assets/Custom/Scripts/HUD/StartButton.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 using VRWidgets;
 
-public class StartButton : ButtonToggleBase {
+public class StartButton : ButtonBase {
 	public EnemyManager enemyManager;
 
-	public override void ButtonTurnsOn() {
+	public override void ButtonPressed() {
 		enemyManager.StartRepeatSpawn();
 		Destroy (transform.parent.gameObject);
+		Debug.Log("Pressed");
 	}
-	
-	public override void ButtonTurnsOff() {
+
+	public override void ButtonReleased() {
+		Debug.Log("Released");
 	}
 }
