1ace53b69a35a99dc1543617852c091158a90120
Alexandre Wimmers <adwimmers@ucdavis.edu>
2015-04-30 12:33:48 -0700

    Hands can grab any cloned object (enemies).
    Former-commit-id: abd5722837c287e5cfaedc37c48cff7c7ce5475b

--- project-unity5/Assets/LeapMotion/Scripts/Utils/MagneticPinch.cs
+++ project-unity5/Assets/LeapMotion/Scripts/Utils/MagneticPinch.cs
@@ -15,7 +15,6 @@ public class MagneticPinch : MonoBehaviour {
 
   public const float TRIGGER_DISTANCE_RATIO = 0.7f;
   
-  public string grabbableNames = ".*Enemy.*";
   public float forceSpringConstant = 100.0f;
   public float magnetDistance = 2.0f;
 
@@ -38,7 +37,7 @@ public class MagneticPinch : MonoBehaviour {
       Vector3 new_distance = pinch_position - close_things[j].transform.position;
       if (close_things[j].GetComponent<Rigidbody>() != null && new_distance.magnitude < distance.magnitude &&
           !close_things[j].transform.IsChildOf(transform) &&
-          Regex.IsMatch(close_things[j].name, grabbableNames + "\\(Clone\\)", RegexOptions.IgnoreCase)) {
+          Regex.IsMatch(close_things[j].name, "\\(Clone\\)", RegexOptions.IgnoreCase)) {
         grabbed_ = close_things[j];
         distance = new_distance;
 		grabbed_.GetComponent<Enemy>().grabbed = true;
