e119458db0ec06f52b84872d0674c0f3999597c5
Alexandre Wimmers <adwimmers@ucdavis.edu>
2015-05-05 09:53:04 -0700

    Starting the game no longer destroys the main menu.  We can't destroy it if we intend to be able to restart our games.
    Former-commit-id: 3b32b1b59650c1583dd7a713403a01ca2ce94f7a

--- project-unity5/Assets/Custom/Scripts/HUD/StartButton.cs
+++ project-unity5/Assets/Custom/Scripts/HUD/StartButton.cs
@@ -5,10 +5,16 @@ using VRWidgets;
 public class StartButton : ButtonBase {
 	public GameManager gameManager;
 
+	private GameObject mainMenu;
+
+	void Start() {
+		mainMenu = GameObject.Find ("MainMenu");
+	}
+
 	public override void ButtonPressed() {
 		Debug.Log("Start button pressed");
 		gameManager.StartGame ();
-		Destroy (transform.parent.gameObject);
+		mainMenu.SetActive(false);
 	}
 
 	public override void ButtonReleased() {
