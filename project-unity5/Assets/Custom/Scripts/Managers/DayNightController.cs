f6928cd18cccb8df1589919292e90108d74ef147
Timothy Tong <tktong418@gmail.com>
2015-05-06 01:48:01 -0700

    Using simple day-night cycling
    Former-commit-id: 0a7be0f9b73a13a5464b510111e2816b602a3736

--- project-unity5/Assets/Custom/Scripts/Managers/DayNightController.cs
+++ project-unity5/Assets/Custom/Scripts/Managers/DayNightController.cs
@@ -17,32 +17,10 @@ public class DayNightController : MonoBehaviour {
 	// Update is called once per frame
 	private void Update () {
 		UpdateSunRotation ();
-		UpdateSunIntensity ();
-
-		currentTime += (Time.deltaTime / SECONDS_PER_DAY) * timeMultiplier;
-
-		if (currentTime >= 1) {
-			currentTime = 0;
-		}
 	}
 
 	private void UpdateSunRotation() {
-		sun.transform.localRotation = Quaternion.Euler ((currentTime * 360.0f) - 90, 170, 0);
-	}
-
-	private void UpdateSunIntensity() {
-		float intensityMultiplier = 1.0f;
-
-		if( (currentTime <= 0.23f) || (currentTime >= 0.75f) ) {
-			intensityMultiplier = 0;
-		}
-		else if(currentTime <= 0.25f) {
-			intensityMultiplier = Mathf.Clamp01((currentTime - 0.23f) * (1 / 0.02f));
-		}
-		else if(currentTime >= 0.73f) {
-			intensityMultiplier = Mathf.Clamp01(1 - ((currentTime - 0.73f) * (1 / 0.02f)));
-		}
-
-		sun.intensity = baseIntensity * intensityMultiplier;
+		sun.transform.Rotate (0.1f * timeMultiplier,0,0);
+		//sun.transform.localRotation = Quaternion.Euler ((currentTime * 360.0f) - 90, 170, 0);
 	}
 }
